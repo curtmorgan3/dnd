10ed47108239636cf4d0b0fa7e0d3d4a95298abf
Curt Morgan <curtmorgan3@gmail.com>
2019-01-05 11:34:59 -0500

    Wizard step 1 complete

--- client/src/Components/Characters/NewCharacter/Wizard/Step1.js
+++ client/src/Components/Characters/NewCharacter/Wizard/Step1.js
@@ -6,17 +6,37 @@ export default class Step1 extends React.Component{
 		super(props);
 		this.state = {
 			name: '',
-			class: '',
+			clas: 'barbarian',
 			race: 'human',
-			alignment: ''
+			alignment: 'good',
+			subAlignment: 'lawful'
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.chooseRace = this.chooseRace.bind(this);
+		this.chooseClass = this.chooseClass.bind(this);
+		this.chooseAlignment = this.chooseAlignment.bind(this);
+		this.chooseSubAlignment = this.chooseSubAlignment.bind(this);
+
 	}
 	chooseRace(e){
 		this.setState({
 			race: e
+		})
+	}
+	chooseClass(e){
+		this.setState({
+			clas: e
+		})
+	}
+	chooseAlignment(e){
+		this.setState({
+			alignment: e
+		})
+	}
+	chooseSubAlignment(e){
+		this.setState({
+			subAlignment: e
 		})
 	}
 	handleChange(e){
@@ -31,6 +51,7 @@ export default class Step1 extends React.Component{
 
 	render(){
 		const races = ['Human', 'Dwarf', 'Elf', 'Halfling', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling']
+		const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
 		return(
 			<div>
 				<form>
@@ -45,6 +66,25 @@ export default class Step1 extends React.Component{
 							))}
 						</DropdownButton>
     			</FormGroup>
+					<FormGroup>
+						<DropdownButton onSelect={this.chooseClass} title={this.state.clas.charAt(0).toUpperCase() + this.state.clas.slice(1)}>
+							{classes.map(clas => (
+								<MenuItem eventKey={clas.toLowerCase()}>{clas}</MenuItem>
+							))}
+						</DropdownButton>
+    			</FormGroup>
+					<FormGroup>
+					<DropdownButton onSelect={this.chooseSubAlignment} title={this.state.subAlignment.charAt(0).toUpperCase() + this.state.subAlignment.slice(1)}>
+						<MenuItem eventKey='lawful'>Lawful</MenuItem>
+						<MenuItem eventKey='neutral'>Nuetral</MenuItem>
+						<MenuItem eventKey='chaotic'>Chaotic</MenuItem>
+					</DropdownButton>
+						<DropdownButton onSelect={this.chooseAlignment} title={this.state.alignment.charAt(0).toUpperCase() + this.state.alignment.slice(1)}>
+							<MenuItem eventKey='good'>Good</MenuItem>
+							<MenuItem eventKey='neutral'>Neutral</MenuItem>
+							<MenuItem eventKey='evil'>Evil</MenuItem>
+						</DropdownButton>
+					</FormGroup>
 				</form>
 				<button onClick={()=> this.props.previousStep()}>Back</button>
 				<button onClick={()=> this.props.nextStep()}>Next</button>
